2fae66a35b529f7246a6c90d13b61323c2a030cf
Francis <francisklinck@gmail.com>
2021-10-31 17:03:48 +0100

    stream code parsing instead of keeping program in memory

--- src/bft.cpp
+++ src/bft.cpp
@@ -16,21 +16,20 @@ int main(int argc, char *argv[]) {
         std::ofstream ofstream("out.cpp");
         Parser parser(&logger);
         if (ifstream.good()) {
-            std::vector<Statement*> statements;
-            int i;
-            /* parse the input file */
-            logger.info("Parsing brainfuck");
-            while((i=ifstream.get())!=-1) {
-                statements.push_back(parser.parseStatement((char)i));
-            }
-            /* Generate cpp code */
-            logger.info("Generating cpp");
+            /* Output some definitions */
+            logger.info("Adding definitions");
             for (const std::string &s : base_begin) {
                 ofstream << s << std::endl;
             }
-            for (Statement *s : statements) {
+            /* parse the input file and generate cpp code */
+            int i;
+            logger.info("Parsing brainfuck and generating code");
+            while((i=ifstream.get())!=-1) {
+                Statement* s = parser.parseStatement((char)i);
                 ofstream << s->getCode() << std::endl;
             }
+            /* Output the file ending */
+            logger.info("Adding file end");
             for (const std::string &s : base_end) {
                 ofstream << s << std::endl;
             }
